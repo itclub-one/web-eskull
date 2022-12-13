f6cac7ad862a153af6614b421460a6c26f7bc9bb
Ticlext-Altihaf <yusufsekhanaltaf@smknegeri1garut.sch.id>
2022-12-13 12:40:32 +0100

    ttt

--- resources/views/admin/validation/login.blade.php
+++ resources/views/admin/validation/login.blade.php
@@ -48,12 +48,7 @@
 
 </div>
 
-<!-- jQuery -->
-<script src="../../plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="../../dist/js/adminlte.min.js"></script>
+
 </body>
 </html>
 
