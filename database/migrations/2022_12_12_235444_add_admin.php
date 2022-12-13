f4dbddcfd454ecfe394b8e17fb55f5c9c553707d
Ticlext-Altihaf <yusufsekhanaltaf@smknegeri1garut.sch.id>
2022-12-13 08:29:58 +0700

    e

--- database/migrations/2022_12_12_235444_add_admin.php
+++ database/migrations/2022_12_12_235444_add_admin.php
@@ -5,6 +5,8 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
+
 //Add admin to users table with default password
 class AddAdmin extends Migration
 {
@@ -17,9 +19,10 @@ class AddAdmin extends Migration
     {
         DB::table('users')->insert([
             'name' => 'admin',
-            'email' => 'admin@localhost',
-            'password' => Hash::make('password'),
+            'email' => 'admin@example.com',
+            'password' => bcrypt('password'),
             'role' => 'admin',
+            'remember_token' => Str::random(60),
         ]);
 
     }
