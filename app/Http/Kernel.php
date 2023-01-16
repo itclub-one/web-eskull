69fc29de592694a27dbe26680908d50a39cbe281
Ticlext-Altihaf <yusufsekhanaltaf@smknegeri1garut.sch.id>
2023-01-16 15:31:03 +0700

    bye

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -30,7 +30,7 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
-            'page-cache' => \Silber\PageCache\Middleware\CacheResponse::class,
+            //'page-cache' => \Silber\PageCache\Middleware\CacheResponse::class,
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
