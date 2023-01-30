ac7a1297a2e09326240fe28fb033156093fbe06f
sanbray99 <ikhsannawawi99@gmail.com>
2023-01-30 14:21:24 +0700

    On branch v2 Your branch is up to date with 'origin/v2'.
    Changes to be committed:
    	modified:   app/Http/Controllers/pendaftaranController.php
    	new file:   public/images/foto-user/osis.png
    	new file:   public/images/foto-user/paskibra.png
    	renamed:    public/images/logo-eskul/root.png -> public/images/foto-user/smea.png
    	new file:   public/images/logo-eskul/paskibra.png
    	new file:   public/images/logo-eskul/smea.png
    	modified:   resources/views/admin/pendaftaran-eskul/pendaftaran.blade.php

--- app/Http/Controllers/pendaftaranController.php
+++ app/Http/Controllers/pendaftaranController.php
@@ -9,139 +9,38 @@ use Illuminate\Support\Str;
 use App\Models\eskul;
 use Illuminate\Http\Request;
 use App\Exports\daftarCalonAnggota;
+use App\Models\role;
 use Maatwebsite\Excel\Facades\Excel;
 
 class pendaftaranController extends Controller
 {
     public function index(Request $request ){
 
-        if($request->has('search')){
+        $pendaftaran = null;
+        $data_eskul = eskul::all();
+        $on = administrator::all();
+        $currentRole = role::where("role", "=", auth()->user()->role)->first();
 
-            $adminmpk = pendaftaran::where('id_eskul','=','1')->get();
-            $adminosis = pendaftaran::where('id_eskul','=','2')->get();
-            $adminirma = pendaftaran::where('id_eskul','=','3')->get();
-            $adminpks = pendaftaran::where('id_eskul','=','4')->get();
-            $adminpramukaputra = pendaftaran::where('id_eskul','=','5')->get();
-            $adminpramukaputri = pendaftaran::where('id_eskul','=','6')->get();
-            $adminpaskibra = pendaftaran::where('id_eskul','=','7')->get();
-            $adminvolly = pendaftaran::where('id_eskul','=','8')->get();
-            $adminfutsalputra = pendaftaran::where('id_eskul','=','9')->get();
-            $adminfutsalputri = pendaftaran::where('id_eskul','=','10')->get();
-            $adminsepakbola = pendaftaran::where('id_eskul','=','11')->get();
-            $adminbasket = pendaftaran::where('id_eskul','=','12')->get();
-            $adminhockey = pendaftaran::where('id_eskul','=','13')->get();
-            $adminbadminton = pendaftaran::where('id_eskul','=','14')->get();
-            $adminkarate = pendaftaran::where('id_eskul','=','15')->get();
-            $admintaekwondo = pendaftaran::where('id_eskul','=','16')->get();
-            $adminsilat = pendaftaran::where('id_eskul','=','17')->get();
-            $admintarungderajat = pendaftaran::where('id_eskul','=','18')->get();
-            $adminkir = pendaftaran::where('id_eskul','=','19')->get();
-            $adminkopsis = pendaftaran::where('id_eskul','=','20')->get();
-            $adminpmr = pendaftaran::where('id_eskul','=','21')->get();
-            $adminsenitari = pendaftaran::where('id_eskul','=','22')->get();
-            $adminmarchingband = pendaftaran::where('id_eskul','=','23')->get();
-            $adminsenikriya = pendaftaran::where('id_eskul','=','24')->get();
-            $adminpaduansuara = pendaftaran::where('id_eskul','=','25')->get();
-            $adminkarawitan = pendaftaran::where('id_eskul','=','26')->get();
-            $adminteater = pendaftaran::where('id_eskul','=','27')->get();
-            $adminsenimusik = pendaftaran::where('id_eskul','=','28')->get();
-            $adminitclub = pendaftaran::where('id_eskul','=','29')->get();
-            $adminbroadcast = pendaftaran::where('id_eskul','=','30')->get();
-            $adminenglishclub = pendaftaran::where('id_eskul','=','31')->get();
-            $adminkoreanclub = pendaftaran::where('id_eskul','=','32')->get();
-            $adminjapanclub = pendaftaran::where('id_eskul','=','33')->get();
-            $admindeutschclub = pendaftaran::where('id_eskul','=','34')->get();
+        if ($request->has('search')) {
+            if (auth()->user()->role == "root") {
+                $pendaftaran = pendaftaran::where('nama_calon_anggota', 'like', '%' . $request->search . '%')
+                    ->orWhere('id_eskul', 'like', '%' . $request->search . '%')->paginate(5);
+            } else {
+                $pendaftaran = pendaftaran::where('id_eskul', '=', $currentRole['eskul_id'])->where('nama_calon_anggota', 'like', '%' . $request->search . '%')
+                    ->orWhere('id_eskul', 'like', '%' . $request->search . '%')->paginate(5);
+            }
+        } else {
+            if (auth()->user()->role == "root") {
+                $pendaftaran = pendaftaran::paginate(10);
+            } else {
 
-
-            $data = pendaftaran::where('nama_calon_anggota','like', '%' .$request->search. '%')
-                ->orWhere('id_eskul','like', '%' .$request->search. '%')->paginate(5);
-
-            $data_eskul = eskul::all();
-            $on = administrator::all();
-
-
-        }else{
-            $adminmpk = pendaftaran::where('id_eskul','=','1')->get();
-            $adminosis = pendaftaran::where('id_eskul','=','2')->get();
-            $adminirma = pendaftaran::where('id_eskul','=','3')->get();
-            $adminpks = pendaftaran::where('id_eskul','=','4')->get();
-            $adminpramukaputra = pendaftaran::where('id_eskul','=','5')->get();
-            $adminpramukaputri = pendaftaran::where('id_eskul','=','6')->get();
-            $adminpaskibra = pendaftaran::where('id_eskul','=','7')->get();;
-            $adminvolly = pendaftaran::where('id_eskul','=','8')->get();
-            $adminfutsalputra = pendaftaran::where('id_eskul','=','9')->get();
-            $adminfutsalputri = pendaftaran::where('id_eskul','=','10')->get();
-            $adminsepakbola = pendaftaran::where('id_eskul','=','11')->get();
-            $adminbasket = pendaftaran::where('id_eskul','=','12')->get();
-            $adminhockey = pendaftaran::where('id_eskul','=','13')->get();
-            $adminbadminton = pendaftaran::where('id_eskul','=','14')->get();
-            $adminkarate = pendaftaran::where('id_eskul','=','15')->get();
-            $admintaekwondo = pendaftaran::where('id_eskul','=','16')->get();
-            $adminsilat = pendaftaran::where('id_eskul','=','17')->get();
-            $admintarungderajat = pendaftaran::where('id_eskul','=','18')->get();
-            $adminkir = pendaftaran::where('id_eskul','=','19')->get();
-            $adminkopsis = pendaftaran::where('id_eskul','=','20')->get();
-            $adminpmr = pendaftaran::where('id_eskul','=','21')->get();
-            $adminsenitari = pendaftaran::where('id_eskul','=','22')->get();
-            $adminmarchingband = pendaftaran::where('id_eskul','=','23')->get();
-            $adminsenikriya = pendaftaran::where('id_eskul','=','24')->get();
-            $adminpaduansuara = pendaftaran::where('id_eskul','=','25')->get();
-            $adminkarawitan = pendaftaran::where('id_eskul','=','26')->get();
-            $adminteater = pendaftaran::where('id_eskul','=','27')->get();
-            $adminsenimusik = pendaftaran::where('id_eskul','=','28')->get();
-            $adminitclub = pendaftaran::where('id_eskul','=','29')->get();
-            $adminbroadcast = pendaftaran::where('id_eskul','=','30')->get();
-            $adminenglishclub = pendaftaran::where('id_eskul','=','31')->get();
-            $adminkoreanclub = pendaftaran::where('id_eskul','=','32')->get();
-            $adminjapanclub = pendaftaran::where('id_eskul','=','33')->get();
-            $admindeutschclub = pendaftaran::where('id_eskul','=','34')->get();
-
-
-
-            $data = pendaftaran::orderBy('created_at', 'DESC')->paginate(5);
-            $data_eskul = eskul::all();
-            $on = administrator::all();
-
+                $pendaftaran = pendaftaran::where('id_eskul', '=', $currentRole['eskul_id'])->get();
+            }
         }
         // dd($data);
         return view('admin.pendaftaran-eskul.pendaftaran', compact(
             'on',
-            'data',
-            'data_eskul',
-            'adminmpk',
-            'adminosis',
-            'adminirma',
-            'adminpks',
-            'adminpramukaputra',
-            'adminpramukaputri',
-            'adminpaskibra',
-            'adminvolly',
-            'adminfutsalputra',
-            'adminfutsalputri',
-            'adminsepakbola',
-            'adminbasket',
-            'adminhockey',
-            'adminbadminton',
-            'adminkarate',
-            'admintaekwondo',
-            'adminsilat',
-            'admintarungderajat',
-            'adminkir',
-            'adminkopsis',
-            'adminpmr',
-            'adminsenitari',
-            'adminmarchingband',
-            'adminsenikriya',
-            'adminpaduansuara',
-            'adminkarawitan',
-            'adminteater',
-            'adminsenimusik',
-            'adminitclub',
-            'adminbroadcast',
-            'adminenglishclub',
-            'adminkoreanclub',
-            'adminjapanclub',
-            'admindeutschclub',
+            'pendaftaran',
             )) ;
     }
 
